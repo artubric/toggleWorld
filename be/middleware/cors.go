196ff36998f03a6027db4d765b5623606d54806a
artubric <bricis.artur@gmail.com>
2021-02-17 11:39:58 +0200

    Update cors.go
    typo
    POSY -> POST

--- be/middleware/cors.go
+++ be/middleware/cors.go
@@ -7,7 +7,7 @@ func Cors(handler http.Handler) http.Handler {
 	return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
 		w.Header().Add("Access-Control-Allow-Origin", "*")
 		w.Header().Add("Content-Type", "application/json")
-		w.Header().Add("Access-Control-Allow-Methods", "POSY, GET, OPTIONS, PUT, DELETE")
+		w.Header().Add("Access-Control-Allow-Methods", "POST, GET, OPTIONS, PUT, DELETE")
 		w.Header().Add("Access-Control-Allow-Headers", "Accept, Content-Type, Content-Length")
 		handler.ServeHTTP(w, r)
 	})
